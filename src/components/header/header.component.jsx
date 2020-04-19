09f7472c120592fe10b5eca86da12936b9204011
Tamás Czabai <czabait@gmail.com>
2020-04-19 19:38:34 +0200

    add react redux (to header)

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { auth } from '../../firebase/firebase.utils';
+import { connect } from 'react-redux';
 
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 import './header.styles.scss';
 
-const Header = ({ currentUser }) => (
-    <div className='header'>
+const Header = ({ currentUser }) => {
+    return (<div className='header'>
         <Link className='logo-container' to='/'>
             <Logo className='logo'/>
         </Link>
@@ -28,7 +29,11 @@ const Header = ({ currentUser }) => (
                 </Link>
             }
         </div>
-    </div>
-)
+    </div>);
+}
 
-export default Header;
+const mapStateToProps = state => ({
+    currentUser: state.user.currentUser
+});
+
+export default connect(mapStateToProps)(Header);
